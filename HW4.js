1453a539dec795d830534658bc3901fcc158c740
Vinsensius Yuda Pratama <pantkrauss@gmail.com>
2024-04-20 12:13:23 +0700

    Update HW4.js
    fungsi generator nilai random

--- HW4.js
+++ HW4.js
@@ -1,1 +1,24 @@
 // tes pertama
+// random nomor generator 1-50
+const array = 100;
+
+const arraytotal = [];
+const arrayindeksgenap = [];
+const arrayindeksganjil = [];
+
+for (let i = 0; i < array; i++) {
+    const nilairandom= Math.floor(Math.random() * 50) + 1;
+    arraytotal.push(nilairandom);
+  }
+  
+console.log(arraytotal)
+
+// fungsi min
+
+// fungsi max
+
+// fungsi total
+
+// fungsi rata rata
+
+// fungsi main 
