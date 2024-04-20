2e7c6e1bc7ef02fe027f3ccf5789fe949e108940
Vinsensius Yuda Pratama <pantkrauss@gmail.com>
2024-04-20 12:42:02 +0700

    split array
    split array total, ke array indeks genap dan array indeks ganjil

--- HW4.js
+++ HW4.js
@@ -3,16 +3,27 @@
 const array = 100;
 
 const arraytotal = [];
-const arrayindeksgenap = [];
-const arrayindeksganjil = [];
 
 for (let i = 0; i < array; i++) {
     const nilairandom= Math.floor(Math.random() * 50) + 1;
     arraytotal.push(nilairandom);
   }
-  
 console.log(arraytotal)
 
+// Pemisah array indeks genap dan array index ganjil
+const arrayindeksgenap = [];
+const arrayindeksganjil = [];
+
+for (let i = 0; i < arraytotal.length; i++ ){
+    if ( i % 2 ===0){// indeks genap
+        arrayindeksgenap.push(arraytotal[i]);
+    } else {// indeks ganjil
+        arrayindeksganjil.push(arraytotal[i]);
+    }
+
+}
+console.log("Array indeks genap:", arrayindeksgenap);
+console.log("Array indeks ganjil:", arrayindeksganjil);
 // fungsi min
 
 // fungsi max
